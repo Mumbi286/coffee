72ab311836b56d09cc2e73ed028f5f7a8e55e6aa
Christine Mumbi <christinemumbi286@gmail.com>
2025-11-25 04:23:48 +0300

    Validates the price and check the instances of the coffee

--- order.py
+++ order.py
@@ -24,4 +24,25 @@ class Order:
     @property
     def order_coffee(self):
         return self._coffee #returns the private coffee instance
+
+    #setter for the coffee
+    @order_coffee.setter
+    def order_coffee(self, value):
+        #we check if the value is an instance of Coffee
+        if not isinstance(value, Coffee):
+            raise ValueError("Order coffee must be a Coffee instance")
+        self._coffee = value #stores the private coffee instance
+
+    @property
+    def order_price(self):
+        return self._price 
+    @order_price.setter
+    def order_price(self, value):
+        if not isinstance(value, (int, float)):
+            raise ValueError("Order price must be a number")
+        # we check if the value is between 0 and 100
+        if not (0 <= value <= 100):
+            raise ValueError("Order price must be between 0 and 100")
+        self._price = value  # stores the private price value
+
     pass
