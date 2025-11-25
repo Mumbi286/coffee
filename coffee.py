689b472aaeec85521cc45148d4804fda8339b32d
Christine Mumbi <christinemumbi286@gmail.com>
2025-11-25 03:58:34 +0300

    Initialized coffee and implemented the property name

--- coffee.py
+++ coffee.py
@@ -1,2 +1,15 @@
 class Coffee:
+    def __init__(self, name):
+        self.name = name
+    @property
+    def coffee_name(self):
+        return self.name
+
+    @coffee_name.setter
+    def coffee_name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Coffee name must be a string")
+        if len(value) < 3:
+            raise ValueError("Coffee name must be between 1 and 50 characters")
+        self.name = value
     pass
